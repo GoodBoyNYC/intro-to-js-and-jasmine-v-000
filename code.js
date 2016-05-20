378690929d1f46067eb4800f8e5f29a33e4286fe
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-20 16:57:18 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(icecream){
+  return "I love " + icecream
+}
+
+function shouting(word){
+  return word.toUpperCase()
+}
+
+function roundDown(numb){
+  return Math.floor(numb)
+}
+
+function theTruth(){
+  return true
+}
